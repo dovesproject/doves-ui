ad831880a5c8bcff303b0a315c19ff765513e61c
Matthew Horridge <matthew.horridge@stanford.edu>
2023-03-07 10:36:02 -0800

    Single token service

--- src/app/token.service.ts
+++ src/app/token.service.ts
@@ -14,25 +14,9 @@ export class TokenService {
   constructor(private http: HttpClient) { }
 
   getTokens(endPoint : string, query : string) : Observable<Term []> {
-    // if(serviceUrl.includes("conditions")) {
-      // return diseases.filter((disease) => disease.name.startsWith(query)).map((disease) => disease.name);
-    // }
-    // else if(serviceUrl.includes("outcomes")) {
-     const params = new HttpParams().append("query", query);
-     let response = this.http.get<Term []>("http://localhost:8080" + endPoint, {params : params});
-     console.log(response);
-     return response;
-     
-    // }
-    // else if(serviceUrl.includes("specialties")) {
-    //   return specialties.filter((specialty) => specialty.startsWith(query));
-    // }
-    // else if(serviceUrl.includes("procedures")) {
-    //   return ["Office visit", "Administer flu shot"];
-    // }
-    // else {
-    //   return [];
-    // }
+    const params = new HttpParams().append("query", query);
+    let response = this.http.get<Term []>("http://localhost:8080" + endPoint, {params : params});
+    return response;
   }
 
   isIncluded(disease : Disease, query: string) : boolean {
